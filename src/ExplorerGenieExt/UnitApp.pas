fdaba690120527acc6ff994ae0dfb9f263581540
martinstoeckli <developer@martinstoeckli.ch>
2020-11-14 23:00:33 +0100

    Added MPL2 license information to the ExplorerGenieExt project.

--- src/ExplorerGenieExt/UnitApp.pas
+++ src/ExplorerGenieExt/UnitApp.pas
@@ -1,3 +1,8 @@
+// Copyright © 2020 Martin Stoeckli.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 unit UnitApp;
 
 {$WARN SYMBOL_PLATFORM OFF}
